eab96630bc45e701e1de8ff880adf3094162fff6
CherryLU1993 <32975719+CherryLU1993@users.noreply.github.com>
2021-04-16 02:04:31 +0900

    Practice 1,2

--- code/src/com/allendowney/thinkdast/MyArrayList.java
+++ code/src/com/allendowney/thinkdast/MyArrayList.java
@@ -44,8 +44,16 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public boolean add(T element) {
-		// TODO: FILL THIS IN!
-		return false;
+		if (size >= array.length) {
+			// make a bigger array and copy over the elements
+			@SuppressWarnings("unchecked")
+			T[] bigger = (T[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, bigger, 0, array.length);
+			array = bigger;
+		}
+		array[size] = element;
+		size++;
+		return true;
 	}
 
 	@Override
@@ -110,7 +118,12 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public int indexOf(Object target) {
-		// TODO: FILL THIS IN!
+		for (int i = 0; i < size; i++) {
+			if (equals(target, array[i])) {
+				return i;
+			}
+		}
+
 		return -1;
 	}
 
@@ -119,7 +132,7 @@ public class MyArrayList<T> implements List<T> {
 	 * Handles the special case that the target is null.
 	 *
 	 * @param target
-	 * @param object
+	 * @param element
 	 */
 	private boolean equals(Object target, Object element) {
 		if (target == null) {
@@ -181,8 +194,12 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public T remove(int index) {
-		// TODO: FILL THIS IN!
-		return null;
+		T old = get(index);
+		for (int i=index; i<size-1; i++) {
+			array[i] = array[i+1];
+		}
+		size--;
+		return old;
 	}
 
 	@Override
@@ -201,8 +218,13 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public T set(int index, T element) {
-		// TODO: FILL THIS IN!
-		return null;
+//		if (index < 0 || index >= size()) {
+//			throw new IndexOutOfBoundsException();
+//		} //todo: why returns old?
+
+		T old = get(index);
+		array[index] = element;
+		return old;
 	}
 
 	@Override
